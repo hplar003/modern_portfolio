e963b044bb2bb93eee631c503c5cc23281ff19c3
ralphmaronilla <ralphmaronilla003@gmail.com>
2022-07-05 11:20:32 +0800

    fix mobile tech stacks

--- src/themes/default.js
+++ src/themes/default.js
@@ -17,7 +17,7 @@ export default {
   // Breakpoints for responsive design
   breakpoints: {
 
-    sm: 'screen and (max-width: 340px)',
+    sm: 'screen and (max-width: 640px)',
     md: 'screen and (max-width: 768px)',
     lg: 'screen and (max-width: 1024px)',
     xl: 'screen and (max-width: 1280px)'
